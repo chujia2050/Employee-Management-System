b16b2d1f4c31d5e1783c3d729d6328948c1447fc
chenc <cjchen122720@gmail.com>
2023-10-10 14:37:14 -0700

    second commit

---
+++ src/main/java/com/example/employeemanagementsystem/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.employeemanagementsystem.service;
+
+import com.example.employeemanagementsystem.entity.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+}
